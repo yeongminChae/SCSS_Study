2784b6d3487d2700ed6a68251d4baf5ab874ef61
채영민 <94206927+yeongminChae@users.noreply.github.com>
2022-12-19 17:00:29 +0000

    week 2 day 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,117 @@
 import "./App.css";
-import photo1 from "./image/photo1.png"
-import photo2 from "./image/photo2.png"
-import photo3 from "./image/photo3.png"
-import photo4 from "./image/photo4.png"
 
 export default function App() {
   return (
-    <div className="flex flex-col min-w-full max-w-full mt-20 flex justify-center items-center">
-      <span className="font-semibold text-4xl">
-        Foreword, Contributors
-      </span>
-      <div className="grid grid-cols-3 md:grid-cols-6 w-[95%] sm:w-[85%] md:w-[93vw] lg:w-[90vw] xl:w-[85vw] h-full mt-5 ">
-        <div className="w-full h-56 flex flex-col items-center bg-[#C7BCA1] " >
-          <span className="text-sm mt-3 w-[80%] text-center ">
-            Tools for Slowing Down
-          </span>
-          <span className="text-xs text-center relative -bottom-32">
-            Chloe Geogheaan & Chloe Reith
-          </span>
+    <div>
+      <div className="bg-black w-screen h-screen">
+        <div className="flex justify-center items-center w-full h-full">
+          <div className="h-[80%] w-[95%] bg-white mb-10 xl:mt-10 rounded-lg xl:w-[80%] xl:h-[65%] " >
+            <div className="w-full h-8 bg-gray-300 rounded-t-lg flex items-center">
+              <div className="w-20 h-8 flex">
+                <div className="ml-4 space-x-2 flex items-center justify-center ">
+                  <div className="rounded-full w-3 h-3 border border-black/40 border-[1.5px]" />
+                  <div className="rounded-full w-3 h-3 border border-black/40 border-[1.5px]" />
+                  <div className="rounded-full w-3 h-3 border border-black/40 border-[1.5px]" />
+                </div>
+              </div>
+              <div className="w-full h-8 flex items-center justify-center">
+                <div className="w-80 rounded-md mr-10 h-5 border border-black/40 border-[1.5px]" />
+              </div>
+            </div>
+            <div className="grid grid-cols-2 w-full h-full">
+              <div className="grid grid-cols-2 w-full h-full">
+                <div className="bg-black text-white pt-2 pl-3 text-lg leading-4">
+                  CHYNDES<br/>
+                  CHANGES<br/>
+                  CHUNDES<br/>
+                  CHANGES<br/>
+                  JULY 4-5
+                </div>
+                <div className="bg-violet-600"></div>
+                <div className="row-span-2 bg-red-600 pt-1 pl-3">
+                  ABOUT<br/>
+                  CONTACT<br/>
+                  VENUES<br/>
+                  FAQ<br/>
+                  PARTNERS
+                </div>
+                <div className="bg-white "></div>
+                <div className="bg-yellow-400 mt-6 pt-1 pl-3">
+                  TICKETS
+                </div>
+              </div>
+              <div className="bg-white grid grid-cols-2 p-3 w-full h-full ">
+                <div className="flex flex-col">
+                  <span className="text-xs mb-2">
+                  SPEAKERS
+                  </span>
+                  <div className="text-sm "> 
+                  ABBY GEE<br/>
+                  ALEX YABSLEY<br/>
+                  ALEX ZACCARIA<br/>
+                  ALLEGRA CALDWELL<br/>
+                  BEATRICE<br/>
+                  <span className="bg-violet-600">
+                    BILL WILSON [Us]<br/>
+                  </span>
+                  BRIGID DIXON <br/> CARLY JAMES [US] CHERIE HU [US]<br/>
+                  CHRIS MYLRAE<br/>
+                  CHRISTIE MORGAN<br/>
+                  CHRISTOPHER<br/>
+                  MURATORE [US]<br/>
+                  DANIELLE LEVY [Us]<br/>
+                  DAVE "DIZZ1" NORRIS<br/>
+                  DAVID BENGE [NZ]<br/>
+                  DAVID RAY <br/>
+                  GARETH SMITH [US]<br/>
+                  JENN PELLY [USI<br/>
+                  JOSEPH EDWARD<br/>
+                  KEYES [US]<br/>
+                  KAT DOPPER<br/>
+                  KATE DUNCAN<br/>
+                  LORRAE MCKENNA MARIHUZKA<br/>
+                  CORNELIUS<br/>
+                  MARKELL CASEY [US]
+                  </div>
+                </div>
+                <div className="flex flex-col">
+                  <span className="text-xs mb-2">
+                   ARTISTS
+                  </span>
+                  <div className="text-sm "> 
+                  808S AND GREATEST
+                  HITS<br/>
+                  ACTIVITIES OF DAILY
+                  LIVING<br/>
+                  ALEXANDER BIGGS<br/>
+                  ALICE SKYE<br/>
+                  AUSMUTEANTS<br/>
+                  BABY<br/>
+                  BENCH PRESS<br/>
+                  BIRDZ<br/>
+                  BONES AND JONES<br/>
+                  CABLE TIES<br/>
+                  CHAPEL<br/>
+                  CIVIC<br/>
+                  CLEOPOLD<br/>
+                  DARCY BAYLIS<br/>
+                  DARK FAIR<br/>
+                  DIANAS<br/>
+                  DIET<br/>
+                  DRMNGNOW<br/>
+                  EAST BRUNSWICK ALL<br/>
+                  GIRLS CHOIR<br/>
+                  EILISH GILLIGAN<br/>
+                  ELIZABETH<br/>
+                  EVAN KLAR<br/>
+                  EVELYN IDA MORRIS<br/>
+                  FRASER A. GORMAN
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="w-full h-56 flex flex-col bg-[#] " >
-          <img src={photo1} className="h-56" />
-        </div>
-        <div className="w-full h-56 flex flex-col items-center bg-[#FF6D28] " >
-          <span className="text-sm mt-3 w-[80%] text-center ">
-            The (Old) New Spirit of Curating and Mvths of Nomadism
-          </span>
-          <span className="text-xs text-center relative -bottom-24">
-            Tim Gentles
-          </span></div>
-        <div className="w-full h-56 flex flex-col bg-[#] " >
-           <img src={photo2} />
-        </div>
-        <div className="w-full h-56 flex flex-col items-center bg-[#6B728E] " >
-          <span className="text-sm mt-3 w-[80%] text-center ">
-            Pressing Singularities
-          </span>
-          <span className="text-xs text-center relative -bottom-32">
-            Tendai John Mutambu AKobvn Maree Pickens
-          </span></div>
-        <div className="w-full h-56 flex flex-col items-center bg-[#B3FFAE] " >
-          <span className="text-sm mt-3 w-[80%] text-center ">
-            We're in This Together
-          </span>
-          <span className="text-xs text-center relative -bottom-36">
-            Vera Mey
-          </span></div>
-        <div className="w-full h-56 flex flex-col items-center bg-[#A62349] " >
-          <span className="text-sm mt-3 w-[80%] text-center ">
-            The Curatorial as a Liveable Subject Position: Hospitality and Differential Consciousness
-          </span>
-          <span className="text-xs text-center relative -bottom-[3.5rem] ">
-            Danny Butt
-          </span></div>
-        <div className="w-full h-56 flex flex-col items-center bg-[#82C3EC] " >
-          <span className="text-sm mt-3 w-[80%] text-center ">
-            Community, Community Art, Communitv Art in Howick
-          </span>
-          <span className="text-xs text-center relative -bottom-24">
-            Balamohan Shingade
-          </span></div>
-        <div className="w-full h-56 flex flex-col bg-[#] " >
-           <img src={photo3} className="h-56" />
-        </div>
-        <div className="w-full h-56 flex flex-col items-center bg-[#5F8D4E] " >
-          <span className="text-sm mt-3 w-[80%] text-center ">
-            Feeling Welcome?
-          </span>
-          <span className="text-xs text-center relative -bottom-36">
-            Louisa Afoa & loana Gordon-Smith
-          </span></div>
-        <div className="w-full h-56 relative flex flex-col bg-[#] " >
-           <img src={photo4} className="object-cover h-56 " />
-        </div>
-        <div className="w-full h-56 flex flex-col items-center bg-[#F3CCFF] " >
-          <span className="text-sm mt-3 w-[80%] text-center ">
-            On Friendship
-          </span>
-          <span className="text-xs text-center relative -bottom-40">
-            Rebecca Boswell
-          </span></div>
       </div>
     </div>
   );
